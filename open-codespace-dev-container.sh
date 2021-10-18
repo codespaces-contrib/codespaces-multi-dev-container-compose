dab41338f4a03a2a9201bb8998939144cbce57a8
Chuck Lantz <clantz@microsoft.com>
2021-10-18 11:34:46 -0700

    Improvements

--- open-codespace-dev-container.sh
+++ open-codespace-dev-container.sh
@@ -5,10 +5,13 @@ cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 DEVCONTAINER_BASE_FOLDER="${1:-"."}"
 export DOCKER_HOST="tcp://localhost:${2:-9256}"
-BOOTSTRAP_ROOT_FOLDER="${3:-"/workspace"}"
 
 workspace_container="$(docker ps -q --filter "label=com.github.codespaces.active.workspace=true")"
-export COMPOSE_PROJECT_NAME="$(docker inspect -f '{{ index .Config.Labels "com.docker.compose.project" }}' ${workspace_container})"
+COMPOSE_PROJECT_NAME="$(docker inspect -f '{{ index .Config.Labels "com.docker.compose.project" }}' ${workspace_container})"
+if [ ! -z "${COMPOSE_PROJECT_NAME}" ]; then
+    export COMPOSE_PROJECT_NAME
+fi
+workspace_folder_in_container="$(docker exec -i ${workspace_container} /bin/sh -c 'cat /tmp/__boostrap_container_workspace_folder')"
 
 temp_dir="$(pwd)/._devcontainer_temp"
 mkdir -p "${temp_dir}"
@@ -18,6 +21,7 @@ cat << 'EOF' > "${temp_dir}/.gitignore"
 EOF
 
 echo "Boostrap container ID: ${workspace_container}"
+echo "Workspace folder in container: ${workspace_folder_in_container}"
 echo "Temp directory: ${temp_dir}"
 
 # Copy config files
@@ -25,12 +29,14 @@ echo
 echo "Copying:"
 while IFS= read -r content_path; do
     echo "- ${content_path}"
-    docker cp -L "${workspace_container}:${BOOTSTRAP_ROOT_FOLDER}/${content_path}" "${temp_dir}"
+    docker cp -L "${workspace_container}:${workspace_folder_in_container}/${content_path}" "${temp_dir}" 2>/dev/null || echo "   (Skipped, does not exist.)"
 done < common-config.list
 echo "- ${DEVCONTAINER_BASE_FOLDER}/.devcontainer"
 target_base_folder="${temp_dir}/${DEVCONTAINER_BASE_FOLDER}"
 mkdir -p "${target_base_folder}"
-docker cp -L "${workspace_container}:${BOOTSTRAP_ROOT_FOLDER}/${DEVCONTAINER_BASE_FOLDER}/.devcontainer" "${target_base_folder}"
+docker cp -L "${workspace_container}:${workspace_folder_in_container}/${DEVCONTAINER_BASE_FOLDER}/.devcontainer" "${target_base_folder}"
+
+export SECONDARY_CODESPACE_CONTAINER=true
 
 echo
 echo "Launching VS Code..."
