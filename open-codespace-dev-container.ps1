0d2858ef83020053be85dce429d59d4780a56d50
Chuck Lantz <clantz@microsoft.com>
2021-10-18 20:31:57 -0700

    Fix

--- open-codespace-dev-container.ps1
+++ open-codespace-dev-container.ps1
@@ -12,13 +12,13 @@ Write-Host "Boostrap container ID: $workspaceContainer"
 $composeProjectName = (docker inspect -f '{{ index .Config.Labels \"com.docker.compose.project\" }}' ${workspaceContainer}) -join ''
 if ($composeProjectName -ne "" ) {
     Write-Host "Compose project name: $composeProjectName"
-    $env:COMPOSE_RPOEJCT_NAME=$composeProjectName
+    $env:COMPOSE_PROJECT_NAME=$composeProjectName
 }
 $workspaceFolderInContainer = (docker exec -i $workspaceContainer /bin/sh -c "cat /tmp/__boostrap_container_workspace_folder") -join ''
 Write-Host "Workspace folder in container: $workspaceFolderInContainer"
 
 $tempDir = "$pwd/._devcontainer_temp"
-New-Item -ItemType Directory -Force -Path "$tempDir"
+New-Item -ItemType Directory -Force -Path "$tempDir" > $null
 Write-Host "Temp directory: $tempDir"
 
 # Add a .gitignore file
@@ -29,16 +29,29 @@ $tempGitIgnore = "
 [IO.File]::WriteAllText("$tempDir/.gitignore", $tempGitIgnore)
 
 # Copy config files
-$commonConfigList = Get-Content -Path "$PSScriptRoot\common-config.list"
+$targetBaseFolder="$tempDir\$devContainerBaseFolder"
+New-Item -ItemType Directory -Force -Path "$targetBaseFolder" > $null
+Write-Host
 Write-Host "Copying:"
+Write-Host "- $devContainerBaseFolder/.devcontainer"
+Try {
+    docker cp -L "${workspaceContainer}:${workspaceFolderInContainer}/${devContainerBaseFolder}/.devcontainer" "${targetBaseFolder}"
+} Catch {
+    Write-Host "Failed to copy .devcontainer folder. Valid path?"
+    exit 1
+}
+$commonConfigList = Get-Content -Path "$PSScriptRoot\common-config.list"
 ForEach ($contentPath in $commonConfigList) {
     Write-Host "- $contentPath"
-    docker cp -L "${workspaceContainer}:${workspaceFolderInContainer}/${contentPath}" "${tempDir}"
+    Try {
+        docker cp -L "${workspaceContainer}:${workspaceFolderInContainer}/${contentPath}" "${tempDir}" 2>$null
+    } Catch {
+        Write-Host "   (Skipping $contentPath. Not found.)"
+    }
 }
-Write-Host "- $devContainerBaseFolder/.devcontainer"
-$targetBaseFolder="$tempDir/$devContainerBaseFolder"
-New-Item -ItemType Directory -Force -Path "$targetBaseFolder"
-docker cp -L "${workspaceContainer}:${workspaceFolderInContainer}/${devContainerBaseFolder}/.devcontainer" "${targetBaseFolder}"
 
+Write-Host
 Write-Host "Launching VS Code..."
 code --force-user-env --disable-workspace-trust --skip-add-to-recently-opened "${targetBaseFolder}"
+
+
